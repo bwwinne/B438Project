adb079fb573bad319db3e10a90894c6eed9f0c6f
Bryce <bwwinne@gmail.com>
2019-11-26 15:38:24 -0500

    player movement and walking animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        //Debug.Log(change);
+
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            anim.SetFloat("moveX", change.x);
+            anim.SetFloat("moveY", change.y);
+            anim.SetBool("moving", true);
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
